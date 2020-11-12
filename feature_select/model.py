04c63a8acbd2b55edd691ed1c51085aab4407266
hsj <hsjfans.scholar@gmail.com>
2020-11-12 08:49:25 +0800

    back

--- feature_select/model.py
+++ feature_select/model.py
@@ -9,10 +9,11 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import f1_score, precision_recall_fscore_support
 from matplotlib import pyplot as plt
-from multiprocessing import Process
+from multiprocessing import Pool
 
 # from featexp import get_univariate_plots#用于特征筛选，需要先安装featexp
 warnings.filterwarnings("ignore")
+
 plt.rcParams['font.sans-serif'] = ['Simhei']
 plt.rcParams['axes.unicode_minus'] = False
 
@@ -75,7 +76,7 @@ def cat_serachParm(train_data, train_labels, cat_features):
     #
     # 55, 60, 70, 80
     for iter_cnt in [55, 60, 70, 80]:
-        print('iter_cnt:', iter_cnt)
+        print('cat_serachParm iter_cnt:', iter_cnt)
         for lr in [0.03, 0.035, 0.040, 0.045, 0.050, 0.055, 0.060, 0.065]:
             for max_depth in [5, 6, 7, 8]:
                 clf = cat_model(iter_cnt, lr, max_depth, cat_features)
@@ -85,7 +86,7 @@ def cat_serachParm(train_data, train_labels, cat_features):
                     param = [iter_cnt, lr, max_depth]
                     best = mean_f1
                     best_model = clf
-                    print(param, best)
+                    print('cat_serachParm 搜索最佳参数 ', param, best)
     print('cat_serachParm 搜索最佳参数 ....... over', param, best)
     return best_model, param, best
 
@@ -105,7 +106,7 @@ def rf_searchParam(train_data, train_labels):
     best = 0
     best_model = None
     for n_estimators in [50, 55, 57, 60, 65]:
-        print('n_estimators:', n_estimators)
+        print('rf_searchParam n_estimators:', n_estimators)
         for min_samples_split in [6, 8, 10, 13, 15, 17, 20]:
             for max_depth in [11, 12, 13, 15]:
                 rf = rf_model(n_estimators, max_depth, min_samples_split)
@@ -115,7 +116,7 @@ def rf_searchParam(train_data, train_labels):
                     param = [n_estimators, min_samples_split, max_depth]
                     best = mean_f1
                     best_model = rf
-                    print(param, best)
+                    print('rf_searchParam 搜索最佳参数', param, best)
     print('rf_searchParam 搜索最佳参数 ....... over', param, best)
     return best_model, param, best
 
@@ -145,7 +146,7 @@ def lgb_searchParam(train_data, train_labels):
     best_model = None
     # 40, 45, 50, 55, 60, 65, 70
     for n_estimators in [40, 45, 50, 55, 60, 65, 70]:
-        print('n_estimators:', n_estimators)
+        print('lgb_searchParam n_estimators:', n_estimators)
         for max_depth in [6, 7, 8]:
             for num_leaves in [40, 45, 50, 55, 60, 65, 70]:
                 for learning_rate in [0.01, 0.03, 0.05, 0.08, 0.1, 0.15, 0.2, 0.25]:
@@ -158,7 +159,7 @@ def lgb_searchParam(train_data, train_labels):
                                  num_leaves, learning_rate]
                         best = mean_f1
                         best_model = lgb_cl
-                        print(param, best)
+                        print('lgb_searchParam 搜索最佳参数', param, best)
     print('lgb_searchParam 搜索最佳参数 ....... over', param, best)
     return best_model, param, best
 
@@ -181,7 +182,8 @@ def predict(name, model, train_data, train_label, test_data, merge=False, n_spli
         model.fit(x_train, y_train)
         pred_cab = model.predict(x_valid)
         f1_score_ = eval_score(y_valid, pred_cab)['f1']
-        print('model = {} 第{}次验证的f1:{}'.format(model, i + 1, f1_score_))
+        print(name, merge_name, 'model = {} 第{}次验证的f1:{}'.format(
+            model, i + 1, f1_score_))
         feature_importance = pd.DataFrame({
             'column': train_data.columns,
             'importance': model.feature_importances_,
@@ -190,19 +192,20 @@ def predict(name, model, train_data, train_label, test_data, merge=False, n_spli
         mean_f1 += f1_score_ / n_splits
         ans = model.predict_proba(test)
         answers.append(ans)
-    print('mean f1:', mean_f1)
+    print(name, merge_name, 'mean f1:', mean_f1)
     feature_importances = pd.concat(feature_importance_list)
     feature_importances = feature_importances.groupby(
         'column')['importance'].agg('mean').sort_values(ascending=False).reset_index()
     feature_importances.to_csv(
-        f'../feature_importance/{name}_{merge_name}_feature_importance.csv')
+        f'../feature_importance/{name}_{merge_name}_feature_importance.csv', index=False)
     prediction = np.sqrt(sum(np.array(answers)**2) / n_splits)[:, 1]
     predict_res = pd.DataFrame(
         {'id': test_data['id'],
          "score": prediction}
     )
-    predict_res.to_csv(f'../prediction/{name}_{merge_name}_prediction.csv')
-    return prediction, feature_importances
+    predict_res.to_csv(
+        f'../prediction/{name}_{merge_name}_prediction.csv', index=False)
+    return prediction, feature_importances, mean_f1
 
 
 def get_fearures():
@@ -236,39 +239,73 @@ def get_topK_features(feature_importance_list, k=20):
     return topk_feature_names
 
 
+def cat_(train_data, train_labels, test_data, cat_features, merge):
+    best_cat, _, _ = cat_serachParm(train_data, train_labels, cat_features)
+
+    cat_score, cat_feature_importances, mean_f1 = predict('cat',
+                                                          best_cat, train_data, train_labels, test_data, merge=merge)
+    return cat_score, cat_feature_importances, mean_f1
+
+
+def rf_(train_data, train_labels, test_data, merge):
+    best_rf, _, _ = rf_searchParam(train_data, train_labels)
+    rf_score, rf_feature_importances, mean_f1 = predict('rf',
+                                                        best_rf, train_data, train_labels, test_data, merge=merge)
+    return rf_score, rf_feature_importances, mean_f1
+
+
+def lgb_(train_data, train_labels, test_data, merge):
+    best_lgb, _, _ = lgb_searchParam(train_data, train_labels)
+    lgb_score, lgb_feature_importances, mean_f1 = predict('lgb',
+                                                          best_lgb, train_data, train_labels, test_data, merge=merge)
+    return lgb_score, lgb_feature_importances, mean_f1
+
+
 def train(train_data, train_labels, test_data, cat_features, merge=False):
     merge_name = ''
     if merge:
         merge_name = 'merge'
 
-    best_cat = cat_serachParm(train_data, train_labels, cat_features)
+    pool = Pool(3)
+    results = []
+    results.append(pool.apply_async(func=cat_, args=(
+        train_data, train_labels, test_data, cat_features, merge)))
+    results.append(pool.apply_async(func=rf_, args=(
+        train_data, train_labels, test_data, merge)))
+    results.append(pool.apply_async(func=lgb_, args=(
+        train_data, train_labels, test_data, merge)))
+    pool.close()
+    pool.join()
+    feature_importance_list = []
+    scores = []
+    mean_f1_scores = []
+    for res in results:
+        score, feature_importance, mean_f1 = res.get()
+        scores.append(score)
+        feature_importance_list.append(feature_importance)
+        mean_f1_scores.append(mean_f1)
 
-    cat_score, cat_feature_importances = predict('cat',
-                                                 best_cat, train_data, train_labels, test_data, merge=merge)
-    best_rf = rf_searchParam(train_data, train_labels)
-    rf_score, rf_feature_importances = predict('rf',
-                                               best_rf, train_data, train_labels, test_data, merge=merge)
-    best_lgb = lgb_searchParam(train_data, train_labels)
-    lgb_score, lgb_feature_importances = predict('lgb',
-                                                 best_lgb, train_data, train_labels, test_data, merge=merge)
-
-    final_score = (rf_score + cat_score + lgb_score) / 3.0
+    max_id, min_id = np.argmax(mean_f1_scores), np.argmin(mean_f1_scores)
+    final_score = 3 / 6 * scores[max_id] + 1 / 6 * \
+        scores[min_id] + 2 / 6 * scores[3 - max_id - min_id]
     test_data['score'] = final_score  # 可选:fina_persudo是伪标签的预测结果
     submit_csv = test_data[['id', 'score']]
     submit_csv.to_csv(f'../submit_{merge_name}.csv', index=False)
-    return cat_feature_importances, rf_feature_importances, lgb_feature_importances
+    return feature_importance_list
 
 
 if __name__ == "__main__":
     train_data, train_labels, test_data, cat_features = get_fearures()
     cat_feature_importances, rf_feature_importances, lgb_feature_importances = train(
         train_data, train_labels, test_data, cat_features)
+    k = 25
     topk_feature_names = get_topK_features(
-        [cat_feature_importances, rf_feature_importances, lgb_feature_importances])
+        [cat_feature_importances, rf_feature_importances, lgb_feature_importances], k)
     train_data = train_data[topk_feature_names]
     new_cat_features = topk_feature_names.intersection(cat_features)
     topk_feature_names.add('id')
+    print(' select topK {} train ---------------------'.format(k))
     train(train_data,
           train_labels, test_data[topk_feature_names], new_cat_features, merge=True)
-
+    print(' finsh --------------------------------')
     # %%
